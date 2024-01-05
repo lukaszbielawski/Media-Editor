dd9d02e89946cb4ea41b2b3ee846d0859ac89d94
lukaszbielawski <lukasz.bi@onet.pl>
2024-01-05 17:34:46 +0100

    Initial Commit

---
+++ Media-Editor/Media_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Media_EditorApp.swift
+//  Media-Editor
+//
+//  Created by Łukasz Bielawski on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Media_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
